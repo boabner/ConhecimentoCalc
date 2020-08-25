9e2db0fc95d3b32a6de3b1acbe26790be678da45
Abner <abner@intersite.com.br>
2020-08-25 10:46:46 -0400

    FreteFiscal

--- exampleComRestFul.py
+++ exampleComRestFul.py
@@ -7,50 +7,51 @@ app = Flask(__name__)
 api = Api(app)
 
 desenvolvedores = [
-    {'nome': 'Abner',
-     'habilidades': ['Python', 'Flask']
-     },
-    {'nome': 'Fulano',
-     'habilidades': ['Java', 'PHP']
-     }
+  {'nome': 'Abner',
+   'habilidades': ['Python', 'Flask']
+   },
+  {'nome': 'Fulano',
+   'habilidades': ['Java', 'PHP']
+   }
 ]
+
 
 class Desenvolvedor(Resource):
 
-    def get(self, id):
-        try:
-            response = desenvolvedores[id]
-        except IndexError:
-            response = {
-                'status': 'erro',
-                'mensagem:': 'Desenvolvedor de id {} não existe'.format(id)
-            }
-        return response
+  def get(self, id):
+    try:
+      response = desenvolvedores[id]
+    except IndexError:
+      response = {
+        'status': 'erro',
+        'mensagem:': 'Desenvolvedor de id {} não existe'.format(id)
+      }
+    return response
 
-    def put(self, id):
-        dados = json.loads(request.data)
-        desenvolvedores[id] = dados
-        return dados
+  def put(self, id):
+    dados = json.loads(request.data)
+    desenvolvedores[id] = dados
+    return dados
 
-    def post(self):
-        pass
+  def post(self):
+    pass
 
-    def delete(self, id):
-        desenvolvedores.pop(id)
-        return {'status': 'sucesso', 'mensagem': 'Registro excluído'}
+  def delete(self, id):
+    desenvolvedores.pop(id)
+    return {'status': 'sucesso', 'mensagem': 'Registro excluído'}
 
 
 class ListaDesenvolvedores(Resource):
 
-    def get(self):
-        return desenvolvedores
+  def get(self):
+    return desenvolvedores
 
-    def post(self):
-        dados = json.loads(request.data)
-        desenvolvedores.append(dados)
-        return {
-            'status': 'sucesso', 'mensagem': 'registros inseridos'
-        }
+  def post(self):
+    dados = json.loads(request.data)
+    desenvolvedores.append(dados)
+    return {
+      'status': 'sucesso', 'mensagem': 'registros inseridos'
+    }
 
 
 api.add_resource(Desenvolvedor, '/dev/<int:id>/')
@@ -58,4 +59,4 @@ api.add_resource(ListaDesenvolvedores, '/dev/')
 api.add_resource(CalcValorFreteFiscal, '/habilidades/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
