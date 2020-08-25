a3903325061e754f1b812add48271c406f322c69
Abner <abner@intersite.com.br>
2020-08-25 07:23:44 -0400

    bd

--- app.py
+++ app.py
@@ -49,23 +49,23 @@ class CalcAdiantamentoEmpresa(Resource):
 
 class CalcValorPedagio(Resource):
 
-    def post(self):
-        dados = json.loads(request.data)
-        print(dados)
-        #
-        pedagioCalcEixo = dados['pedagioCalcEixo']
-        precoTonPedagio = float(dados['precoTonPedagio'])
-        quantidadeEixos = float(dados['quantidadeEixos'])
-        #
-        if pedagioCalcEixo == 'S':
-            resultado = precoTonPedagio * quantidadeEixos
-        else:
-            pesoSaida = dados['pesoSaida']
-            resultado = precoTonPedagio * (pesoSaida/100)
-        #
-        return {
-            'status': 'sucesso', 'resultado': str(resultado)
-        }
+  def post(self):
+    dados = json.loads(request.data)
+    print(dados)
+    #
+    pedagioCalcEixo = dados['pedagioCalcEixo']
+    precoTonPedagio = float(dados['precoTonPedagio'])
+    quantidadeEixos = float(dados['quantidadeEixos'])
+    #
+    if pedagioCalcEixo == 'S':
+      resultado = precoTonPedagio * quantidadeEixos
+    else:
+      pesoSaida = dados['pesoSaida']
+      resultado = precoTonPedagio * (pesoSaida / 100)
+    #
+    return {
+      'status': 'sucesso', 'resultado': str(resultado)
+    }
 
 
 api.add_resource(CalcValorMerc, '/calcValorMerc/')
